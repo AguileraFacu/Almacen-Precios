b222eb8fe7a203421ee2251d933b2f9b5cfee22c
AguileraFacu <facundoaguileradev@gmail.com>
2025-01-11 14:26:35 -0300

    update settings

--- almacen_precios/almacen_precios/settings.py
+++ almacen_precios/almacen_precios/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'almacen_precios.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
